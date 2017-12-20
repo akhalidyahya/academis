24e9bad0673b867555220c78f063468120da4150
RULNJT0PZN1BSFU\Administrator <khalid.yahya9@gmail.com>
2017-12-20 11:42:35 +0700

    view table data guru

--- application/views/pages/admin_view_data_guru.php
+++ application/views/pages/admin_view_data_guru.php
@@ -2,18 +2,44 @@
 						<div class="page-header">
 							<h1>
 								Data Guru
-								<small>
-									<i class="ace-icon fa fa-angle-double-right"></i>
-									data guru
-								</small>
 							</h1>
+							<div style="margin:15px 0px 0px 0px;">
+								<button class="btn btn-primary btn-xs" type="button" name="button">Tambah data</button>
+							</div>
 						</div><!-- /.page-header -->
 
 						<div class="row">
 							<div class="col-xs-12">
 								<!-- PAGE CONTENT BEGINS -->
-								<table>
-
+								<table class="table">
+									<thead>
+										<td>No</td>
+										<td>Nama</td>
+										<td>Alamat</td>
+										<td>Kelurahan</td>
+										<td>Kecamatan</td>
+										<td>Provinsi</td>
+										<td>Jenis Kelamin</td>
+										<td>TTL</td>
+										<td>NIP</td>
+										<td>Email</td>
+										<td>No. Telpon</td>
+										<td>action</td>
+									</thead>
+									<tbody>
+										<td>1</td>
+										<td>Data Dummy</td>
+										<td>Data Dummy</td>
+										<td>Data Dummy</td>
+										<td>Data Dummy</td>
+										<td>Data Dummy</td>
+										<td>Data Dummy</td>
+										<td>Data Dummy</td>
+										<td>Data Dummy</td>
+										<td>Data Dummy</td>
+										<td>Data Dummy</td>
+										<td> <i class="fa fa-edit"></i> <i class="fa fa-times"></i> <i class="fa fa-book"></i> <i class="fa fa-users"></i> </td>
+									</tbody>
 								</table>
 								<!-- PAGE CONTENT ENDS -->
 							</div><!-- /.col -->
