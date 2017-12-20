79efa3ec27d22f6b65015d93923b5ce89cc5b92f
RULNJT0PZN1BSFU\Administrator <khalid.yahya9@gmail.com>
2017-12-20 08:26:24 +0700

    login

--- application/views/pages/admin_view_data_guru.php
+++ application/views/pages/admin_view_data_guru.php
@@ -12,7 +12,9 @@
 						<div class="row">
 							<div class="col-xs-12">
 								<!-- PAGE CONTENT BEGINS -->
-								
+								<table>
+
+								</table>
 								<!-- PAGE CONTENT ENDS -->
 							</div><!-- /.col -->
 						</div><!-- /.row -->
